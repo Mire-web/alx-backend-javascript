907ddebb9db5b22a35930b826634e5e52caf0f6e
mire-web <amahe8664@gmail.com>
2024-08-05 18:44:44 +0100

    let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
 
   if (trueOrFalse) {
     const task = false;
-    const task2 = false;
+    const task2 = true;
   }
 
   return [task, task2];
