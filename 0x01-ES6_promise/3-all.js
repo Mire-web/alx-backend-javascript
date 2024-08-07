8e38c8a38f94ba7c661ec09fe1d89f48f83c1a93
mire-web <amahe8664@gmail.com>
2024-08-07 15:55:38 +0100

    util9

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((data) => {
     console.log(data[0].body, data[1].firstName, data[1].lastName);
-  });
+  }).catch(() => new Error('Signup system offline'));
 }
