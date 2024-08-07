32a23c52d135c1cd064453f919ea0def3f7fa6ee
mire-web <amahe8664@gmail.com>
2024-08-07 14:58:04 +0100

    response

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export default function handleResponseFromAPI(promise) {
+  promise.then((response) => {
+    console.log('Got a response from API');
+  })
+    .catch(() => new Error());
+}
