5bf22f9220aa8e1977dd24a033a68e5ee540b420
mire-web <amahe8664@gmail.com>
2024-08-07 14:19:48 +0100

    1-promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+/* eslint-disable prefer-promise-reject-errors */
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject({ Error: 'The fake API is not working currently' });
+  });
+  return promise;
+}
