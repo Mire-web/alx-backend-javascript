65116f5753bf958d77a4e5ae09b48eec06eb5236
mire-web <amahe8664@gmail.com>
2024-08-07 14:03:10 +0100

    0-promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    resolve('Hello');
+  });
+  return promise;
+}
