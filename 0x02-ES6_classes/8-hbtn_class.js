cab6dd7fd6af21f82b01c911a82a5aa16fca91e8
mire-web <amahe8664@gmail.com>
2024-08-09 01:16:24 +0100

    hbtn_class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof (size) === 'number') this._size = size;
+    else throw new TypeError('Size must be number');
+    if (typeof (location) === 'string') this._location = location;
+    else throw new TypeError('Location must be string');
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
