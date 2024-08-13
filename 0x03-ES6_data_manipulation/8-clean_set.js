a1015350f59fccd75dc069a2967dee86846e50bf
mire-web <amahe8664@gmail.com>
2024-08-13 23:45:19 +0100

    clen

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-expressions */
 export default function cleanSet(set, startString) {
   let str = '';
+  if (startString === '') return '';
   set.forEach((element) => {
-    if (element.slice(0, startString.length) === startString && startString !== '') str === '' ? str = element.slice(startString.length) : str += `-${element.slice(startString.length)}`;
+    if (element.slice(0, startString.length) === startString) str === '' ? str = element.slice(startString.length) : str += `-${element.slice(startString.length)}`;
   });
   return str;
 }
