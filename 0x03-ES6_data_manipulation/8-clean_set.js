211bc09df24d8f7217252a75c81b8b0cfbc47353
mire-web <amahe8664@gmail.com>
2024-08-13 23:35:39 +0100

    clean

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,8 @@
+/* eslint-disable no-unused-expressions */
 export default function cleanSet(set, startString) {
   let str = '';
-  for (const value of set) {
-    const cutStr = value.slice(0, startString.length);
-    if (cutStr === startString && startString !== '') {
-      if (str === '') str = value.slice(startString.length);
-      else str += `-${value.slice(startString.length)}`;
-    }
-  }
+  set.forEach((element) => {
+    if (element.slice(0, startString.length) === startString && startString !== '') str === '' ? str = element.slice(startString.length) : str += `-${element.slice(startString.length)}`;
+  });
   return str;
 }
