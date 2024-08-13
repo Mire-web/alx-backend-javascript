b318245c3d6aea340fc7983e8fee9bbf0e1058bc
mire-web <amahe8664@gmail.com>
2024-08-14 00:19:20 +0100

    glist

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mmap = new Map();
+  mmap.set('Apples', 10);
+  mmap.set('Tomatoes', 10);
+  mmap.set('Pasta', 1);
+  mmap.set('Rice', 1);
+  mmap.set('Banana', 5);
+  return mmap;
+}
