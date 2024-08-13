ed53999bd8fb351aa68c077a6c9dc997e1ee57b5
mire-web <amahe8664@gmail.com>
2024-08-14 00:36:53 +0100

    handlee-erroe

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,9 @@
 export default function updateUniqueItems(mmap) {
-  for (const [key, value] of mmap) {
-    if (value === 1) {
-      try {
-        mmap.set(key, 100);
-      } catch (err) {
-        throw new Error('Cannot process');
-      }
+  if (mmap instanceof Map) {
+    for (const [key, value] of mmap) {
+      if (value === 1) mmap.set(key, 100);
     }
+    return;
   }
+  throw new Error('Cannot process');
 }
