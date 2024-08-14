c4a325c5a3e3cbb58ca0097938b424f6cce7ae0b
mire-web <amahe8664@gmail.com>
2024-08-14 01:15:15 +0100

    fix-eslint-error

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
-    let count = 0;
-    if (weakMap.get(endpoint)) count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
-    if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
-}
+  let count = 0;
+  if (weakMap.get(endpoint)) count = weakMap.get(endpoint);
+  weakMap.set(endpoint, count + 1);
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+};
