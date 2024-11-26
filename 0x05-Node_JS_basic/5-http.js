545cfa8d92955dda248f2e1f1793c7b3fd978c30
mire-web <amahe8664@gmail.com>
2024-11-26 13:06:25 +0100

    endpoint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,10 +1,51 @@
-const http = require('http')
-const students = require('./3-read_file_async.js')
+const http = require('http');
+const { readFile } = require('fs');
 
 const host = '127.0.0.1';
-const PORT = 1245
+const PORT = 1247;
 
-const app = http.createServer((req, res) => {
+
+function countStudents(fileName) {
+	const students = {};
+	const fields = {};
+	let length = 0;
+	return new Promise((resolve, reject) => {
+	  readFile(fileName, (error, data) => {
+		if (error) {
+		  reject(Error('Cannot load the database'));
+		} else {
+		  const lines = data.toString().split('\n');
+		  for (let i = 0; i < lines.length; i += 1) {
+			if (lines[i]) {
+			  length += 1;
+			  const field = lines[i].toString().split(',');
+			  if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+				students[field[3]].push(field[0]);
+			  } else {
+				students[field[3]] = [field[0]];
+			  }
+			  if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+				fields[field[3]] += 1;
+			  } else {
+				fields[field[3]] = 1;
+			  }
+			}
+		  }
+		  const l = length - 1;
+		  output = `Number of students: ${l}`;
+		  for (const [key, value] of Object.entries(fields)) {
+			if (key !== 'field') {
+			  output += '\n';
+			  output += `Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`;
+			}
+		  }
+		  resolve(output);
+		}
+	  });
+	});
+  }
+
+const app = http.createServer(async (req, res) => {
 	const {url, method} = req;
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
@@ -12,10 +53,17 @@ const app = http.createServer((req, res) => {
 	if(url === '/' && method === 'GET'){
 		res.end('Hello Holberton School!')
 	} else if(url === '/students' && method === 'GET'){
-		res.end(students(process.argv[2]))
+		res.write('This is the list of our students\n');
+		countStudents(process.argv[2].toString()).then((output) => {
+			const out = output.slice(0, -1);
+			res.end(out);
+		}).catch (() => {
+			res.statusCode = 404;
+			res.end('Cannot load the database');
+		});
 	}
-})
+});
 
 app.listen(PORT, host, () => {
-	console.log('listening on 1245')
-})
+	console.log('listening on 1245');
+});
