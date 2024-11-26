87c071e667338c468b2ed9f725afd41a14498544
mire-web <amahe8664@gmail.com>
2024-11-26 12:31:25 +0100

    http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,12 @@
 const http = require('http')
 
-const PORT = 1245
+const port = 1245
 const app = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.end('Hello Holberton School!')
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello Holberton School!');
 })
 
-app.listen(PORT, '127.0.0.1', () => {})
+app.listen(port, '127.0.0.1', () => {})
+
+module.exports = app;
