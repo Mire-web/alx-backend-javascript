1bf35192dc71da60aff38ca7ed27e3943db9d2b6
mire-web <amahe8664@gmail.com>
2024-11-26 12:12:23 +0100

    check-now

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.setEncoding("utf8");
 process.stdin.on('data', (chunk) => {
-	process.stdout.write("Your name is: " + chunk);
+	process.stdout.write("Your name is: " + chunk.toString());
 	process.exit();
 })
 process.on('exit', () => {
