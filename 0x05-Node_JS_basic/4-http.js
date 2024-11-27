7d5b5cfaf243d748cb4fe10f6597c1fbe50266b9
mire-web <amahe8664@gmail.com>
2024-11-27 12:46:06 +0100

    fix-4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,8 +5,8 @@ const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-})
+});
 
-app.listen(port, '127.0.0.1', () => {})
+app.listen(port, '127.0.0.1', () => {});
 
 module.exports = app;
