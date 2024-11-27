b3a51253f8fecab13bca4522748586c67bf6c7e0
mire-web <amahe8664@gmail.com>
2024-11-27 12:40:35 +0100

    fix-1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,16 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.setEncoding("utf8");
-process.stdin.on('data', (chunk) => {
-	process.stdout.write("Your name is: " + chunk.toString());
-	process.exit();
-})
-process.on('exit', () => {
-	if (!process.stdin.isTTY)
-		process.stdout.write("This important software is now closing\n");
-})
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
