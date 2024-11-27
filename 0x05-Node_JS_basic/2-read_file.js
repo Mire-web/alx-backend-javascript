fb58c559076c6b8a6548e5936f1331b670ddd63d
mire-web <amahe8664@gmail.com>
2024-11-27 12:43:40 +0100

    fix-2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -36,30 +36,3 @@ function countStudents(fileName) {
 }
 
 module.exports = countStudents;
-// const fs = require('fs');
-
-// function countStudents(path){
-// 	try {
-// 		const data = fs.readFileSync(path, 'utf8');
-// 		const table = data.split('\n')
-// 		const headers = table[0].split(',')
-// 		const body = table.slice(1).map((line) => {
-// 			const row = line.split(',');
-// 			return headers.reduce((obj, header, index) => {
-// 				obj[header.trim()] = row[index].trim();
-// 				return obj;
-// 			}, {});
-// 		});
-// 		console.log("Number of students: " + body.length)
-// 		csList = body.filter(student => student.field === 'CS').map((student) => student.firstname)
-// 		strForm = csList.join(', ')
-// 		console.log("Number of students in CS: " + csList.length + '. List: ' + strForm)
-// 		sweList = body.filter(student => student.field === 'SWE').map((student) => student.firstname)
-// 		strForm = sweList.join(', ')
-// 		console.log("Number of students in SWE: " + sweList.length + '. List: ' + strForm)
-// 	} catch (err){
-// 		throw new Error('Cannot load the database')
-// 	}
-// }
-
-// module.exports = countStudents;
