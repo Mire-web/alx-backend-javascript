0f518786f6a22d75cda344ded86700fca401c266
mire-web <amahe8664@gmail.com>
2024-11-27 12:47:05 +0100

    fix-4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
-const http = require('http')
+const http = require('http');
 
-const port = 1245
+const port = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
