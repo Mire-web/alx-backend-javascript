257d992847dcdb1a166e7491fb485b9a2250cd5f
mire-web <amahe8664@gmail.com>
2024-11-28 13:18:15 +0100

    0-test

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber", function(){
+  it("...", function(){
+    assert.equal(calculateNumber(1, 3), 4)
+  })
+})
